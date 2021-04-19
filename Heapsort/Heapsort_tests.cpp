2d2a5b115df83183147d509f9ce7019285fb466e
abacaba01 <akulov.aa@phystech.edu>
2021-04-19 05:52:28 +0300

    Add files via upload

---
+++ Heapsort/Heapsort_tests.cpp
@@ -0,0 +1,56 @@
+#include "catch.hpp"
+#include <iostream>
+#include "Heapsort.cpp"
+#define CATCH_CONFIG_MAIN
+using namespace std;
+
+
+
+
+TEST_CASE() {
+    int mas[5];
+    mas[1] = 3;
+    mas[2] = 1;
+    mas[3] = 5;
+    mas[4] = 4;
+    mas[0] = 2;
+
+    HeapSort(mas, 1 ,5);
+    REQUIRE(mas[0]==1);
+    REQUIRE(mas[1]==2);
+    REQUIRE(mas[2]==3);
+    REQUIRE(mas[3]==4);
+    REQUIRE(mas[4]==5);
+}
+
+TEST_CASE() {
+    int mas[7];
+    mas[1] = 3;
+    mas[2] = 1;
+    mas[3] = 5;
+    mas[4] = 4;
+    mas[5] = 2;
+    mas[6]= 7;
+    mas[0]=6;
+
+    HeapSort(mas, 2 ,5);
+    REQUIRE(mas[0]==6);
+    REQUIRE(mas[1]==1);
+    REQUIRE(mas[2]==2);
+    REQUIRE(mas[3]==3);
+    REQUIRE(mas[4]==4);
+    REQUIRE(mas[5]==5);
+    REQUIRE(mas[6]==7);
+}
+
+TEST_CASE() {
+    int mas[10];
+    for (int i = 0; i < 10; i++){
+        mas[i]=rand();
+    }
+    HeapSort(mas, 1 ,10);
+    for (int i = 0; i < 9; i++){
+        REQUIRE(mas[i+1] >= mas[i]);
+    }
+}
+
