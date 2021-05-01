7fa0c3d86c4b5afaf0e612cfd5221402ea6ea905
abacaba01 <akulov.aa@phystech.edu>
2021-05-01 19:22:15 +0500

    Add files via upload

---
+++ Benchmarks/Merge_sort_benchmark.cpp
@@ -0,0 +1,265 @@
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
+#include "MERGESORT.h"
+#include "catch.hpp"
+
+TEST_CASE("Benchmark for Merge_sort with sorted mas") {
+    BENCHMARK("Sort 100")
+                {
+                    int b=100;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b; i++) {
+                        a.push_back(i);
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+    BENCHMARK("Sort 1000")
+                {
+                    int b=1000;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b; i++) {
+                        a.push_back(i);
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+    BENCHMARK("Sort 2500")
+                {
+                    int b=2500;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b; i++) {
+                        a.push_back(i);
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+    BENCHMARK("Sort 5000")
+                {
+                    int b=5000;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b; i++) {
+                        a.push_back(i);
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+    BENCHMARK("Sort 7500")
+                {
+                    int b=7500;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b; i++) {
+                        a.push_back(i);
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+    BENCHMARK("Sort 10000")
+                {
+                    int b=10000;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b; i++) {
+                        a.push_back(i);
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+}
+TEST_CASE("Benchmark for Merge_sort with random mas") {
+    BENCHMARK("Sort 100")
+                {
+                    int b=100;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b; i++) {
+                        a.push_back(rand());
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+    BENCHMARK("Sort 1000")
+                {
+                    int b=1000;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b; i++) {
+                        a.push_back(rand());
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+    BENCHMARK("Sort 2500")
+                {
+                    int b=2500;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b; i++) {
+                        a.push_back(rand());
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+    BENCHMARK("Sort 5000")
+                {
+                    int b=5000;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b; i++) {
+                        a.push_back(rand());
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+    BENCHMARK("Sort 7500")
+                {
+                    int b=7500;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b; i++) {
+                        a.push_back(rand());
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+    BENCHMARK("Sort 10000")
+                {
+                    int b=10000;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b; i++) {
+                        a.push_back(rand());
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+}
+TEST_CASE("Benchmark for Merge_sort with quite sorted mas") {
+    BENCHMARK("Sort 100")
+                {
+                    int b = 100;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b/20; i++) {
+                        a.push_back(20) ;
+                    }
+                    for (int i = b/20; i < b; i++) {
+                        a.push_back(i);
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+    BENCHMARK("Sort 1000")
+                {
+                    int b = 1000;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b/20; i++) {
+                        a.push_back(20) ;
+                    }
+                    for (int i = b/20; i < b; i++) {
+                        a.push_back(i);
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+    BENCHMARK("Sort 2500")
+                {
+                    int b = 2500;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b/20; i++) {
+                        a.push_back(20) ;
+                    }
+                    for (int i = b/20; i < b; i++) {
+                        a.push_back(i);
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+    BENCHMARK("Sort 5000")
+                {
+                    int b = 5000;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b/20; i++) {
+                        a.push_back(20) ;
+                    }
+                    for (int i = b/20; i < b; i++) {
+                        a.push_back(i);
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+    BENCHMARK("Sort 7500")
+                {
+                    int b = 7500;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b/20; i++) {
+                        a.push_back(20) ;
+                    }
+                    for (int i = b/20; i < b; i++) {
+                        a.push_back(i);
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+    BENCHMARK("Sort 10000")
+                {
+                    int b = 10000;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b/20; i++) {
+                        a.push_back(20) ;
+                    }
+                    for (int i = b/20; i < b; i++) {
+                        a.push_back(i);
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+}
+TEST_CASE("Benchmark for Merge_sort with mas with same el") {
+    BENCHMARK("Sort 100")
+                {
+                    int b = 100;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b/20; i++) {
+                        a.push_back(i) ;
+                    }
+                    for (int i = b/20; i < b; i++) {
+                        a.push_back(20);
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+    BENCHMARK("Sort 1000")
+                {
+                    int b = 1000;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b/20; i++) {
+                        a.push_back(i) ;
+                    }
+                    for (int i = b/20; i < b; i++) {
+                        a.push_back(20);
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+    BENCHMARK("Sort 2500")
+                {
+                    int b = 2500;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b/20; i++) {
+                        a.push_back(i) ;
+                    }
+                    for (int i = b/20; i < b; i++) {
+                        a.push_back(20);
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+    BENCHMARK("Sort 5000")
+                {
+                    int b = 5000;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b/20; i++) {
+                        a.push_back(i) ;
+                    }
+                    for (int i = b/20; i < b; i++) {
+                        a.push_back(20);
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+    BENCHMARK("Sort 7500")
+                {
+                    int b = 7500;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b/20; i++) {
+                        a.push_back(i) ;
+                    }
+                    for (int i = b/20; i < b; i++) {
+                        a.push_back(20);
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+    BENCHMARK("Sort 10000")
+                {
+                    int b = 10000;
+                    std::vector<uint32_t> a;
+                    for (int i = 0; i < b/20; i++) {
+                        a.push_back(i) ;
+                    }
+                    for (int i = b/20; i < b; i++) {
+                        a.push_back(20);
+                    }
+                    MergeSort(a.begin(), a.end());
+                };
+}
