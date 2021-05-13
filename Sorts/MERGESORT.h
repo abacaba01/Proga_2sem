a2277be36ca6ae1243dc806112863bc1d7c6f2f9
abacaba01 <akulov.aa@phystech.edu>
2021-05-13 10:25:22 +0500

    Add files via upload

---
+++ Sorts/MERGESORT.h
@@ -0,0 +1,42 @@
+#ifndef SORTING_MERGE_SORT_H
+#define SORTING_MERGE_SORT_H
+
+#include <vector>
+
+#include <iostream>
+
+
+template <class TIter, class TCmp>
+void Merge(TIter left_begin, TIter left_end, TIter right_begin, TIter right_end, TIter res_begin, TCmp cmp){
+    while ((left_begin != left_end) && (right_begin != right_end)){
+        if(cmp(*left_begin, *right_begin)){
+            *res_begin = *left_begin;
+            left_begin++;
+            res_begin++;
+        }
+        else{
+            *res_begin = *right_begin;
+            right_begin++;
+            res_begin++;
+        }
+    }
+}
+
+template <class TIter, class TCmp>
+void MSort(TIter res_begin, TIter left, TIter right, TCmp cmp){
+    if (std::distance(left, right)<=1) {
+        return;
+    }
+    TIter middle = left + std::distance(left, right)/2;
+    MSort(res_begin, left, middle, cmp);
+    MSort(res_begin + std::distance(left, middle), middle, right, cmp);
+    Merge(left, middle, middle, right, res_begin, cmp);
+}
+
+template <class TIter, class TCmp=std::less<typename std::iterator_traits<TIter>::value_type>>
+void MergeSort(TIter left, TIter right, TCmp cmp={}){
+    std::vector<typename std::iterator_traits<TIter>::value_type> res(std::distance(left, right));
+    MSort(res.begin(), left, right, cmp);
+}
+
+#endif //SORTING_MERGE_SORT_H
